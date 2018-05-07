77a159bdea6474150024c22952a5bdb86798e381
Lilli Szafranski <lilli@daoofdev.com>
2018-05-07 15:52:34 -0700

    [develop] Initial import.

---
+++ Conductor/Conductor/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Conductor
+//
+//  Created by Lilli Szafranski on 5/7/18.
+//  Copyright © 2018 Dao of Development. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
