4573e13c9b0e0905c85cd8d2bbd29a79e046f588
Lilli Szafranski <lilli@daoofdev.com>
2019-01-05 12:37:35 -0800

    [VIR-4] A few color tweaks.

--- RainbowDisplayTest/old_color_util.ino
+++ RainbowDisplayTest/old_color_util.ino
@@ -2,8 +2,8 @@
 // TODO: Where to do the color correction? Right now array of bytes. Should be to ints?
 byte ColorCorrectLookup_old[256] = {0,	1,	1,	1,	2,	2,	2,	3,	3,	4,	4,	4,	5,	5,	5,	6,	6,	7,	7,	7,	8,	8,	8,	9,	9,	10,	10,	10,	11,	11,	12,	12,	13,	13,	13,	14,	14,	15,	15,	16,	16,	17,	17,	17,	18,	18,	19,	19,	20,	20,	21,	21,	22,	22,	23,	23,	24,	24,	25,	25,	26,	26,	27,	27,	28,	28,	29,	29,	30,	30,	31,	31,	32,	32,	33,	34,	34,	35,	35,	36,	37,	37,	38,	39,	39,	40,	41,	41,	42,	42,	43,	44,	44,	45,	46,	46,	47,	48,	48,	49,	50,	50,	51,	52,	53,	54,	55,	56,	57,	57,	58,	59,	60,	61,	62,	63,	64,	64,	65,	66,	67,	68,	69,	70,	71,	72,	72,	73,	74,	75,	76,	77,	78,	79,	80,	81,	82,	83,	83,	84,	85,	86,	87,	88,	89,	90,	91,	92,	93,	94,	95,	96,	97,	98,	99,	100,	101,	102,	103,	105,	106,	107,	108,	109,	110,	111,	112,	114,	115,	116,	117,	118,	119,	120,	121,	123,	124,	125,	126,	128,	129,	130,	131,	133,	134,	135,	137,	138,	139,	140,	142,	143,	144,	146,	147,	148,	149,	151,	152,	153,	155,	156,	158,	159,	161,	163,	164,	166,	167,	169,	171,	172,	174,	175,	177,	179,	180,	182,	183,	185,	187,	188,	190,	191,	193,	195,	197,	199,	201,	203,	205,	207,	209,	211,	213,	215,	217,	219,	221,	223,	225,	227,	229,	231,	233,	235,	237,	239,	241,	243,	245,	247,	249,	251,	253,	255};
 
-int makeColor(HSV hsv) {
-    return hsvToRgbInt(hsv);// makeColor(hsl.h, hsl.s, hsl.l);
+int makeColor_old(HSV hsv) {
+    return hsvToRgbInt_old(hsv);// makeColor(hsl.h, hsl.s, hsl.l);
 }
 
 // Convert HSL (Hue, Saturation, Lightness) to RGB (Red, Green, Blue)
@@ -15,7 +15,7 @@ int makeColor(HSV hsv) {
 //
 //   lightness:  0 to 100 - how light the color is, 100=white, 50=color, 0=black
 //
-int makeColor(unsigned int hue, unsigned int saturation, unsigned int lightness)
+int makeColor_old(unsigned int hue, unsigned int saturation, unsigned int lightness)
 {
     unsigned int red, green, blue;
     unsigned int var1, var2;
@@ -43,15 +43,15 @@ int makeColor(unsigned int hue, unsigned int saturation, unsigned int lightness)
         var1 = lightness * 200 - var2;
 
         // TODO: Adding the color correction here doesn't look right. My guess is rounding/truncating issues? Confirm and fix by using RED_GRADIENT_MODE background mode
-        red   = /*ColorCorrectLookup[min(*/h2rgb(var1, var2, (hue < 240) ? hue + 120 : hue - 240) * 255 / 600000/*, BYTE_MAX)]*/;
-        green = /*ColorCorrectLookup[min(*/h2rgb(var1, var2, hue) * 255 / 600000/*, BYTE_MAX)]*/;
-        blue  = /*ColorCorrectLookup[min(*/h2rgb(var1, var2, (hue >= 120) ? hue - 120 : hue + 240) * 255 / 600000/*, BYTE_MAX)]*/;
+        red   = /*ColorCorrectLookup[min(*/h2rgb_old(var1, var2, (hue < 240) ? hue + 120 : hue - 240) * 255 / 600000/*, BYTE_MAX)]*/;
+        green = /*ColorCorrectLookup[min(*/h2rgb_old(var1, var2, hue) * 255 / 600000/*, BYTE_MAX)]*/;
+        blue  = /*ColorCorrectLookup[min(*/h2rgb_old(var1, var2, (hue >= 120) ? hue - 120 : hue + 240) * 255 / 600000/*, BYTE_MAX)]*/;
     }
 
     return (red << 16) | (green << 8) | blue;
 }
 
-unsigned int h2rgb(unsigned int v1, unsigned int v2, unsigned int hue)
+unsigned int h2rgb_old(unsigned int v1, unsigned int v2, unsigned int hue)
 {
     if (hue < 60) return v1 * 60 + (v2 - v1) * hue;
     if (hue < 180) return v2 * 60;
@@ -63,7 +63,7 @@ unsigned int h2rgb(unsigned int v1, unsigned int v2, unsigned int hue)
 // http://forum.pjrc.com/threads/16469-looking-for-ideas-on-generating-RGB-colors-from-accelerometer-gyroscope?p=37170&viewfull=1#post37170
 
 int hsvToRgbInt_old(HSV hsv) {
-    RGB rgb = hsvToRgb(hsv);
+    RGB rgb = hsvToRgb_old(hsv);
 
 #ifdef DEV_RIG
     rgb.r = rgb.r / 4;
